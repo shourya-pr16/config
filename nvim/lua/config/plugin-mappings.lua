ce87c1ebc94cc06b9361adaabe66367a79157393
Shourya Pratap <shouryapratap@Shouryas-MacBook-Air.local>
2026-01-31 01:14:43 +0100

    Added keypress for Lazy git

--- nvim/lua/config/plugin-mappings.lua
+++ nvim/lua/config/plugin-mappings.lua
@@ -1,20 +1,17 @@
 -- telescope related mappings
 require("telescope")
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<C-f>', builtin.git_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', function() 
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<C-f>", builtin.git_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 
-
 -- undotree mapping
-vim.keymap.set('n', '<leader>u', require('undotree').toggle, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>u", require("undotree").toggle, { noremap = true, silent = true })
 
 -- lazy mapping
-vim.keymap.set({ 'n', 'v', 't' }, '<leader>l', "<cmd>:Lazy<CR>")
+vim.keymap.set({ "n", "v", "t" }, "<leader>l", "<cmd>:Lazy<CR>")
 
--- lazy git mappings
--- " setup mapping to call :LazyGit
--- nnoremap <silent> <leader>gg :LazyGit<CR>
--- vim.api.nvim_set_keymap()
+-- lazy git mapping
+vim.keymap.set({ "n" }, "<leader>g", "<cmd>:LazyGit<CR>")
