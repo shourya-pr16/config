6955f745a7a1f03e5a803212bac368ca0c6e2a7b
Shourya Pratap <shouryapratap@Shouryas-MacBook-Air.local>
2026-02-02 15:44:19 +0100

    Updated lua ls

--- nvim/lsp/luals.lua
+++ nvim/lsp/luals.lua
@@ -1,17 +1,6 @@
 -- lua/plugins/mason-tools.lua
 return {
-	"WhoIsSethDaniel/mason-tool-installer.nvim",
-	dependencies = { "mason-org/mason.nvim" },
-	config = function()
-		require("mason-tool-installer").setup({
-			ensure_installed = {
-				"stylua", -- Lua formatter
-				"shfmt", -- Shell formatter
-				"gofumpt", -- Go formatter
-				"goimports", -- Go imports
-			},
-			run_on_start = true, -- install missing tools on startup
-			auto_update = false,
-		})
-	end,
+	cmd = { "lua-language-server" },
+	filetypes = { "lua" },
+	root_markers = { ".luarc.json", ".luarc.jsonc" },
 }
