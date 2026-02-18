ff9a67323633ae70e1c060703ba99f0174e16742
Shourya Pratap <shouryapratap@Shouryas-MacBook-Air.local>
2026-02-18 22:47:44 +0100

    Changed theme and added shortcuts for LSP

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,3 +8,21 @@ vim.lsp.enable("luals")
 vim.lsp.enable("golsp")
 vim.lsp.enable("javalsp")
 vim.lsp.enable("zls_lsp")
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local bufnr = args.buf
+		local opts = { buffer = bufnr, silent = true }
+
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "<leader>f", function()
+			vim.lsp.buf.format({ async = true })
+		end, opts)
+	end,
+})
---
+++ nvim/lua/plugins/nord.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		"shaunsingh/nord.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		config = function()
+			vim.cmd([[colorscheme nord]])
+		end,
+	},
+}
